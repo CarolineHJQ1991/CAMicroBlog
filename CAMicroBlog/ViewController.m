7bfef8fc918e97e3595fe23914015d1d0f1893ec
caroline <caroline.hjq@gmail.com>
2016-03-17 18:44:26 +0800

    Initial Commit

---
+++ CAMicroBlog/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CAMicroBlog
+//
+//  Created by Caroline.H on 3/17/16.
+//  Copyright © 2016 Caroline.H. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
