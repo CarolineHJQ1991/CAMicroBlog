5de9cf3f96d8ceb356d092bfbd82c3470a0a2fa4
caroline <caroline.hjq@gmail.com>
2016-03-18 17:09:18 +0800

    add OAuth2/authorize

--- OAuth认证/OAuth认证/ViewController.m
+++ OAuth认证/OAuth认证/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIWebViewDelegate>
 
 @end
 
@@ -16,9 +16,27 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    UIWebView *webView = [[UIWebView alloc] initWithFrame:self.view.bounds];
+    [self.view addSubview:webView];
+    webView.delegate = self;
+    NSString *urlStr = [NSString stringWithFormat:@"https://api.weibo.com/oauth2/authorize?client_id=%@&redirect_uri=%@",@"3196151688",@"http://www.baidu.com"];
+    NSURL *url = [NSURL URLWithString:urlStr];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [webView loadRequest:request];
+    
 }
-
+#pragma mark - WebView代理方法
+- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
+    NSLog(@"%@",request);
+    NSString *urlStr = request.URL.absoluteString;
+    NSRange range = [urlStr rangeOfString:@"code="];
+    if (range.length == 32) {
+        NSString *code = [urlStr substringFromIndex:range.location+ range.length];
+        NSLog(@"code=%@",code);
+    }
+    
+    return YES;
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
